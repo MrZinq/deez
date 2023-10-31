3b8ca2c588d8165713b123dee200733055a09eef
Parzival <48681663+MrZinq@users.noreply.github.com>
2023-10-31 23:42:08 +0000

    Update database.php

--- config/database.php
+++ config/database.php
@@ -54,9 +54,9 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'mysql'),
+            'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'deez'),
             'password' => env('DB_PASSWORD', 'deez'),
             'charset' => 'utf8',
