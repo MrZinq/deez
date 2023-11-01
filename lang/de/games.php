33eb2435aec052bf8730f7a3bea246a37f2cfece
Parzival <48681663+MrZinq@users.noreply.github.com>
2023-11-01 11:39:50 +0000

    Add files via upload

---
+++ lang/de/games.php
@@ -0,0 +1,61 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Games Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'general' => [
+        'popular_games' => 'Beliebte Spiele',
+        'show_all' => 'Alle Spiele anzeigen',
+    ],
+
+    'overview' => [
+      'all_games' => 'Alle Spiele',
+      'search_result' => 'Suchergebnis für :value',
+      'subheader' => [
+          'also_available' => 'auch verfügbar für',
+          'metascore' => 'Metascore',
+          'userscore' => 'Userscore',
+          'listings' => 'Einträge',
+          'media' => 'Bilder & Videos',
+
+      ],
+    ],
+
+    'add' => [
+      'add_game' => 'Spiel hinzufügen',
+      'search_game' => 'Spiel suchen',
+      'searching' => 'Suche nach Spielen...',
+      'select_system' => 'Plattform auswählen',
+      'select_system_info' => 'Bitte wähle eine Plattform aus',
+      'more' => 'Mehr',
+      'enter_title' => 'Titel eingeben',
+      'search' => 'Suchen',
+      'adding' => 'Spiel hinzufügen zu :Pagename!',
+      'wait' => 'Bitte warten',
+      'platform_not_supported' => 'Plattform wird aktuell nicht unterstützt',
+      'results' => [
+          'in_database' => 'In der Datenbank',
+          'add_tradelist' => 'Zur Liste hinzufügen',
+          'add_database' => 'Zur Datenbank hinzufügen',
+          'details' => 'Details',
+          'no_results' => 'Leider keine Suchergebnisse für :value.',
+      ],
+    ],
+
+    'gallery' => [
+      'close' => 'Schließen (Esc)',
+      'loading' => 'Laden...',
+      'prev' => 'Zurück (Linke Pfeiltaste)',
+      'next' => 'Vor (Rechte Pfeiltaste)',
+      // JS %cur% of %total%
+      'counter' => 'von',
+      'error' => 'Der Inhalt konnte nicht gefunden werden.',
+    ],
+
+
+];
---
+++ lang/de/wishlist.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Wishlist Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'wishlist' => 'Wunschliste',
+    'add_wishlist' => 'Zur Wunschliste hinzufügen',
+    'remove' => 'Entfernen',
+    'remove_wishlist' => 'Von der Wunschliste entfernen',
+    'on_wishlist' => 'Auf deiner Wunschliste',
+    'update' => 'Aktualisieren',
+    'update_wishlist' => 'Wunschliste Aktualisieren',
+
+    'modal' => [
+      'send_notification' => 'Benachrichtigung bekommen',
+      'maximum_price' => 'Maximaler Preis',
+      'maximum_price_hint' => 'Leer lassen, um eine Benachrichtigung für jeden :Game_name Eintrag zu bekommen.',
+    ],
+
+    /* Alerts */
+    'alert' => [
+      'saved' => ':Game_name Wunschlisten Eintrag aktualisiert!',
+      'removed' => ':Game_name von Wunschliste entfernt!',
+      'added' => ':Game_name zur Wunschliste hinzugefügt!',
+      'exists' => ':Game_name ist bereits auf deiner Wunschliste!',
+    ],
+];
